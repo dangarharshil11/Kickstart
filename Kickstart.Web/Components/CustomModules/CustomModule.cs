6a9a06b7cf03d99dab029e5fbe4b8b8a2da6bb82
hdangar <hdangar@synoptek.com>
2025-03-26 14:59:11 +0530

    Implemented Azure Blob Storage

---
+++ Kickstart.Web/Components/CustomModules/CustomModule.cs
@@ -0,0 +1,36 @@
+﻿using CMS;
+using CMS.DataEngine;
+using CMS.IO;
+using Kentico.Xperience.AzureStorage;
+using Kickstart.Web.Components.CustomModules;
+
+[assembly: RegisterModule(typeof(CustomModule))]
+
+namespace Kickstart.Web.Components.CustomModules
+{
+    public class CustomModule : Module
+    {
+        public CustomModule()
+        : base("CustomInit")
+        {
+        }
+
+        // Contains initialization code that is executed when the application starts
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            // Creates a new StorageProvider instance for Azure Blob storage
+            var assetsProvider = AzureStorageProvider.Create();
+
+            // Specifies the target container, the provider ensures its existence in the storage account
+            assetsProvider.CustomRootPath = "main";
+
+            // Makes the 'myassetscontainer' container publicly accessible
+            assetsProvider.PublicExternalFolderObject = true;
+
+            // Maps the local directory to the storage provider
+            StorageHelper.MapStoragePath("~/assets", assetsProvider);
+        }
+    }
+}
